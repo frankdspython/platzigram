d5bd6ee0924125209c1e80bdaa18a4f24ba42c13
Francisco Garcia <garciafran@hotmail.com>
2021-01-14 16:47:38 -0500

    urls and views

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -19,5 +19,6 @@ from platzigram import views
 
 urlpatterns = [
     path('hello-world/', views.hello_world),
-    path('hi/', views.hi),
+    path('sorted/', views.sort_integer),
+    path('hi/<str:name>/<int:age>/', views.say_hi)
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -2,11 +2,13 @@
 
 # Django
 from django.http import HttpResponse
+#from django.http import JsonResponse
 
 # Utilities
 from datetime import datetime
-
 import json
+
+
 
 def hello_world(request):
     """Return greeting"""
@@ -15,15 +17,29 @@ def hello_world(request):
     return HttpResponse('Ohhh hi! Current server time is {now}'.format(now=now))
 
 
-def hi(request):
-    """Hi"""
+def sort_integer(request):
+    """Return a JSON response with sorted integer"""
     # import pdb; pdb.set_trace()
-    numbers = '{' + request.GET['numbers'] + '}'
-    
-    y = json.dumps(numbers)
-    a = 2
-    print(type(numbers))
-    print(json.loads(numbers))
+    numbers = request.GET['numbers']
+    mList = [int(e) if e.isdigit() else e for e in numbers.split(',')]
+    mList.sort()
+    data = {
+        'status': 'ok',
+        'numbers': mList,
+        'message': 'Integers sorted successfully'
+    }
+    #numberList = JsonResponse({ "numbers" : mList},json_dumps_params={'indent': 4})
 
-    #numbers.sort()
-    return HttpResponse(numbers)
+    print(data)
+
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+
+def say_hi(request, name, age):
+    """Return a greeting. """
+    if age < 12:
+        message = 'Sorry {}, you are not allowed here'.format(name)
+    else:
+        message = 'Hello, {}! welcome to platzigram'.format(name)
+    
+    return HttpResponse(message)
