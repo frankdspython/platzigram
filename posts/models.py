8c26dd06b75339f8b4ada0dd1b8fcae1e8c1fff6
Francisco Garcia <garciafran@hotmail.com>
2021-01-16 23:23:17 -0500

    Dashboard de Administracion

--- posts/models.py
+++ posts/models.py
@@ -1,25 +1,3 @@
 """Posts models"""
 # Django
-from django.db import models
 
-class User(models.Model):
-    """User mode. """
-
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    is_admin = models.BooleanField(default=False)
-
-    bio = models.TextField(blank=True)
-
-    birthdate = models.DateField(blank=True, null=True)
-
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        """Return email"""
-        return self.email
