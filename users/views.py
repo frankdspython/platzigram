981645837bec35d333f2721d71c2e54242107e33
Francisco Garcia <garciafran@hotmail.com>
2021-01-20 07:42:57 -0500

    signup users

--- users/views.py
+++ users/views.py
@@ -3,6 +3,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# Exceptions
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
 # Create your views here.
 
 def login_view(request):
@@ -17,6 +24,34 @@ def login_view(request):
         else:
             return render(request, 'users/login.html', {'error': 'Ivanlid username and password'})
     return render(request, 'users/login.html')
+
+
+def signup(request):
+    """Sign up view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in use'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return render(request, 'users/login.html')
+
+    return render(request, 'users/signup.html')
 
 @login_required
 def logout_view(request):
