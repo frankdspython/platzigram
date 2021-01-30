c244f63112974abeec5766f639e069767383adcc
Francisco Garcia <garciafran@hotmail.com>
2021-01-30 17:58:52 -0500

    Protecting Profile View, Detail and List View

--- users/views.py
+++ users/views.py
@@ -3,11 +3,34 @@
 # Django
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import DetailView
+
+# Models
+from django.contrib.auth.models import User
+from posts.models import Post
 
 # Forms
 from users.forms import ProfileForm, SignupForm
 
+class UserDetailView(LoginRequiredMixin, DetailView):
+    """User detail"""
+
+    template_name = 'users/detail.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    queryset = User.objects.all()
+    context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        """Add user's posts to context"""
+
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context['posts'] = Post.objects.filter(user=user).order_by('-created')
+        return context
 
 @login_required
 def update_profile(request):
@@ -25,7 +48,8 @@ def update_profile(request):
             profile.picture = data['picture']
             profile.save()
 
-            return redirect('update_profile')
+            url = reverse('users:detail', kwargs={'username': request.user.username})
+            return redirect(url)
 
     else:
         form = ProfileForm()
@@ -49,7 +73,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect('feed')
+            return redirect('posts:feed')
         else:
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
 
@@ -62,7 +86,7 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect('users:login')
     else:
         form = SignupForm()
 
@@ -77,4 +101,4 @@ def signup(request):
 def logout_view(request):
     """Logout a user."""
     logout(request)
-    return redirect('login')
+    return redirect('users:login')
